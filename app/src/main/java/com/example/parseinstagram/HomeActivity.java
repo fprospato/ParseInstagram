d817ba4361743fa27c3bed656f62c28fd834ec66
Francesco Prospato <fprospato@gmail.com>
2019-07-09 12:20:47 -0700

    - Add Posts Fragment - Set Posts Fragment to be first on load

--- app/src/main/java/com/example/parseinstagram/HomeActivity.java
+++ app/src/main/java/com/example/parseinstagram/HomeActivity.java
@@ -1,19 +1,17 @@
 package com.example.parseinstagram;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
-import com.example.parseinstagram.model.Post;
+import com.example.parseinstagram.fragments.ComposeFragment;
+import com.example.parseinstagram.fragments.PostsFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.parse.FindCallback;
-import com.parse.ParseException;
-
-import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -30,48 +28,42 @@ public class HomeActivity extends AppCompatActivity {
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
 
+        setupNavigationBar();
+    }
+
+
+    private void setupNavigationBar() {
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment = new ComposeFragment();
+
                 switch (menuItem.getItemId()) {
                     case R.id.action_home:
+                        //TODO: swap fragment
+                        fragment = new PostsFragment();
                         Toast.makeText(HomeActivity.this, "Home", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.action_compose:
+                        fragment = new ComposeFragment();
                         Toast.makeText(HomeActivity.this, "Compose", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.action_profile:
+                        //TODO: swap fragment
+                        fragment = new ComposeFragment();
                         Toast.makeText(HomeActivity.this, "Profile", Toast.LENGTH_SHORT).show();
                         break;
                     default:
                         break;
                 }
 
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
                 return true;
             }
         });
-    }
 
-
-    private void loadTopPosts() {
-        final Post.Query postsQuery = new Post.Query();
-        postsQuery.getTop().withUser();
-
-        postsQuery.findInBackground(new FindCallback<Post>() {
-            @Override
-            public void done(List<Post> objects, ParseException e) {
-                if (e == null) {
-                    for (int i = 0; i < objects.size(); i++) {
-                        Log.d(TAG, "Post [ " + i + " ] = "
-                                + objects.get(i).getDescription()
-                                + "\nusername = " + objects.get(i).getUser().getUsername()
-                        );
-                    }
-                } else {
-                    Log.e(TAG, "Error getting posts.");
-                    e.printStackTrace();
-                }
-            }
-        });
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
     }
 }
---
+++ app/src/main/java/com/example/parseinstagram/fragments/PostsFragment.java
@@ -0,0 +1,65 @@
+package com.example.parseinstagram.fragments;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.parseinstagram.R;
+import com.example.parseinstagram.model.Post;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+
+import java.util.List;
+
+public class PostsFragment extends Fragment {
+
+    private final static String TAG = "PostsFragment";
+
+
+    private RecyclerView rvPosts;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_posts, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        rvPosts = view.findViewById(R.id.rvPosts);
+
+    }
+
+
+    private void loadTopPosts() {
+        final Post.Query postsQuery = new Post.Query();
+        postsQuery.getTop().withUser();
+
+        postsQuery.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> objects, ParseException e) {
+                if (e == null) {
+                    for (int i = 0; i < objects.size(); i++) {
+                        Log.d(TAG, "Post [ " + i + " ] = "
+                                + objects.get(i).getDescription()
+                                + "\nusername = " + objects.get(i).getUser().getUsername()
+                        );
+                    }
+                } else {
+                    Log.e(TAG, "Error getting posts.");
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
