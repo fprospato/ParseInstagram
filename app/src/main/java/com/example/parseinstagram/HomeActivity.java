4ae56858c6277fb548c22587e236c627bf77fecb
Francesco Prospato <fprospato@gmail.com>
2019-07-09 12:04:10 -0700

    - Add navigation bar - Move all composing a post code from HomeActivity to ComposeFragment

--- app/src/main/java/com/example/parseinstagram/HomeActivity.java
+++ app/src/main/java/com/example/parseinstagram/HomeActivity.java
@@ -1,35 +1,18 @@
 package com.example.parseinstagram;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
+import android.view.MenuItem;
 import android.widget.Toast;
 
-import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
 
 import com.example.parseinstagram.model.Post;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.FindCallback;
 import com.parse.ParseException;
-import com.parse.ParseFile;
-import com.parse.ParseUser;
-import com.parse.SaveCallback;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
@@ -37,15 +20,7 @@ public class HomeActivity extends AppCompatActivity {
     private final static String TAG = "HomeActivity";
     private static final String imagePath = "";
 
-    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
-    public String photoFileName = "photo.jpg";
-    private File photoFile;
-
-    private EditText etDescription;
-    private Button btnCreate;
-    private Button btnRefresh;
-    private Button btnGetImage;
-    private ImageView ivPreview;
+    private BottomNavigationView bottomNavigationView;
 
 
     @Override
@@ -53,69 +28,26 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        etDescription = findViewById(R.id.etDescription);
-        btnCreate = findViewById(R.id.btnCreate);
-        btnRefresh = findViewById(R.id.btnRefresh);
-        btnGetImage = findViewById(R.id.btnGetImage);
-        ivPreview = findViewById(R.id.ivPreview);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
 
-        setupButtons();
-    }
-
-    private void setupButtons() {
-        btnCreate.setOnClickListener(new View.OnClickListener() {
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                if (photoFile == null || ivPreview.getDrawable() == null) {
-                    Toast.makeText(HomeActivity.this, "Need a photo to post", Toast.LENGTH_LONG).show();
-                    Log.e(TAG, "Tried to post without a photo");
-                } else {
-                    final String description = etDescription.getText().toString();
-                    final ParseUser user = ParseUser.getCurrentUser();
-
-                    final ParseFile parseFile = new ParseFile(photoFile);
-
-                    createPost(description, parseFile, user);
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.action_home:
+                        Toast.makeText(HomeActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_compose:
+                        Toast.makeText(HomeActivity.this, "Compose", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_profile:
+                        Toast.makeText(HomeActivity.this, "Profile", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        break;
                 }
-            }
-        });
 
-        btnRefresh.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                loadTopPosts();
-            }
-        });
-
-        btnGetImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                launchCamera();
-            }
-        });
-    }
-
-
-    private void createPost(String description, ParseFile imageFile, ParseUser user) {
-
-        final Post newPost = new Post();
-        newPost.setDescription(description);
-        newPost.setImage(imageFile);
-        newPost.setUser(user);
-
-        newPost.saveInBackground(new SaveCallback() {
-            @Override
-            public void done(ParseException e) {
-                if (e == null) {
-                    Log.d(TAG, "Post successfully saved");
-
-                    etDescription.setText("");
-                    ivPreview.setImageResource(0);
-                } else {
-                    Log.e(TAG, "Error saving post");
-                    e.printStackTrace();
-                    Toast.makeText(getApplicationContext(), "Error creating post", Toast.LENGTH_SHORT).show();
-                }
+                return true;
             }
         });
     }
@@ -141,70 +73,5 @@ public class HomeActivity extends AppCompatActivity {
                 }
             }
         });
-    }
-
-
-    private void launchCamera() {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-        photoFile = getPhotoFileUri(photoFileName);
-
-        Uri fileProvider = FileProvider.getUriForFile(HomeActivity.this, "com.codepath.fileprovider", photoFile);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
-
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-        }
-    }
-
-
-    public File getPhotoFileUri(String fileName) {
-        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
-
-        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
-            Log.d(TAG, "failed to create directory");
-        }
-
-        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
-
-        return file;
-    }
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-
-                //Code for this if statement was told to only copy and not fully understand what's going on
-                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
-
-                File takenPhotoUri = getPhotoFileUri(photoFileName);
-                Bitmap rawTakenImage = BitmapFactory.decodeFile(takenPhotoUri.getPath());
-
-                Bitmap resizedBitmap = BitmapScaler.scaleToFitWidth(rawTakenImage, 400);
-
-                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-
-                resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 40, bytes);
-
-                try {
-                    File resizedFile = getPhotoFileUri(photoFileName + "_resized");
-                    resizedFile.createNewFile();
-                    FileOutputStream fos = new FileOutputStream(resizedFile);
-
-                    fos.write(bytes.toByteArray());
-                    fos.close();
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                ivPreview.setImageBitmap(takenImage);
-            } else {
-                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
-            }
-        }
     }
 }
---
+++ app/src/main/java/com/example/parseinstagram/fragments/ComposeFragment.java
@@ -0,0 +1,198 @@
+package com.example.parseinstagram.fragments;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
+import androidx.fragment.app.Fragment;
+
+import com.example.parseinstagram.BitmapScaler;
+import com.example.parseinstagram.R;
+import com.example.parseinstagram.model.Post;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static android.app.Activity.RESULT_OK;
+
+public class ComposeFragment extends Fragment {
+
+    private final static String TAG = "ComposeFragment";
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
+
+    public String photoFileName = "photo.jpg";
+    private File photoFile;
+
+    private EditText etDescription;
+    private Button btnCreate;
+    private Button btnRefresh;
+    private Button btnGetImage;
+    private ImageView ivPreview;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_compose, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        etDescription = view.findViewById(R.id.etDescription);
+        btnCreate = view.findViewById(R.id.btnCreate);
+        btnRefresh = view.findViewById(R.id.btnRefresh);
+        btnGetImage = view.findViewById(R.id.btnGetImage);
+        ivPreview = view.findViewById(R.id.ivPreview);
+
+        setupButtons();
+    }
+
+
+    private void setupButtons() {
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (photoFile == null || ivPreview.getDrawable() == null) {
+                    Toast.makeText(getContext(), "Need a photo to post", Toast.LENGTH_LONG).show();
+                    Log.e(TAG, "Tried to post without a photo");
+                } else {
+                    final String description = etDescription.getText().toString();
+                    final ParseUser user = ParseUser.getCurrentUser();
+
+                    final ParseFile parseFile = new ParseFile(photoFile);
+
+                    //createPost(description, parseFile, user);
+                }
+            }
+        });
+
+        btnRefresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //loadTopPosts();
+            }
+        });
+
+        btnGetImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchCamera();
+            }
+        });
+    }
+
+
+    private void createPost(String description, ParseFile imageFile, ParseUser user) {
+
+        final Post newPost = new Post();
+        newPost.setDescription(description);
+        newPost.setImage(imageFile);
+        newPost.setUser(user);
+
+        newPost.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d(TAG, "Post successfully saved");
+
+                    etDescription.setText("");
+                    ivPreview.setImageResource(0);
+                } else {
+                    Log.e(TAG, "Error saving post");
+                    e.printStackTrace();
+                    Toast.makeText(getContext(), "Error creating post", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+    private void launchCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        photoFile = getPhotoFileUri(photoFileName);
+
+        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+
+    public File getPhotoFileUri(String fileName) {
+        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(TAG, "failed to create directory");
+        }
+
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+
+                //Code for this if statement was told to only copy and not fully understand what's going on
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+
+                File takenPhotoUri = getPhotoFileUri(photoFileName);
+                Bitmap rawTakenImage = BitmapFactory.decodeFile(takenPhotoUri.getPath());
+
+                Bitmap resizedBitmap = BitmapScaler.scaleToFitWidth(rawTakenImage, 400);
+
+                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+
+                resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 40, bytes);
+
+                try {
+                    File resizedFile = getPhotoFileUri(photoFileName + "_resized");
+                    resizedFile.createNewFile();
+                    FileOutputStream fos = new FileOutputStream(resizedFile);
+
+                    fos.write(bytes.toByteArray());
+                    fos.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                ivPreview.setImageBitmap(takenImage);
+            } else {
+                Toast.makeText(getContext(), "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
